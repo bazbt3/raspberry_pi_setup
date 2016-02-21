adb09c35d3bd7635f46b31139c5092ae24c8ef22
bazbt3 <bazbt3@users.noreply.github.com>
2016-02-21 17:16:26 +0000

    Update pull_blog_build.sh

--- pull_blog_build.sh
+++ pull_blog_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# The above is important, allows the code to run.
+# The above 'shebang' is important, it allows the code to run.
 # In the past I've used `#!/bin/bash` but I've been reading since.
 
 # 
@@ -12,12 +12,14 @@
 # Mentioned in this repo's README.md file.
 
 cd ~/repo/bazbt3.github.io
-# Changes the working folder to 'repo' ready for action!
+# Changes the working folder as required, ready for action!
 # It'll probably be better to change this to one with my account name, needs a check.
 
 git pull git://github.com/bazbt3/bazbt3.github.io.git
 # To pull down the up-to-date remote repo contents and merge them with the local stuff.
 # It presupposed that the repo exists locally - something I will ensure before making this live.
+
+# At this point I need to invoke `/bin/bash --login` to run the script; something I'm hoping the shebang will accomplish later.
 
 jekyll build
 # Builds a copy of the site in the '_site' folder.  If it doesn't exist, Jekyll will create it.
